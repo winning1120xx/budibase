4c58f280731760c4120f85257dfee545caaafb58
Adria Navarro <adria@budibase.com>
2023-05-11 17:14:51 +0200

    Fix tests to use connection string instead of env

--- packages/server/src/sdk/tests/datasources/validators.spec.ts
+++ packages/server/src/sdk/tests/datasources/validators.spec.ts
@@ -1,7 +1,7 @@
 import { SourceName } from "@budibase/types"
 import integrations from "../../../integrations"
 import { GenericContainer } from "testcontainers"
-import { env } from "@budibase/backend-core"
+import { generator } from "@budibase/backend-core/tests"
 
 jest.unmock("pg")
 jest.unmock("mysql2/promise")
@@ -118,18 +118,44 @@ describe("datasource validators", () => {
   describe("couchdb", () => {
     const validator = integrations.getValidator[SourceName.COUCHDB]!
 
+    let url: string
+
+    beforeAll(async () => {
+      const user = generator.first()
+      const password = generator.hash()
+
+      const container = await new GenericContainer("budibase/couchdb")
+        .withExposedPorts(5984)
+        .withEnv("COUCHDB_USER", user)
+        .withEnv("COUCHDB_PASSWORD", password)
+        .start()
+
+      const host = container.getContainerIpAddress()
+      const port = container.getMappedPort(5984)
+
+      await container.exec([
+        `curl`,
+        `-u`,
+        `${user}:${password}`,
+        `-X`,
+        `PUT`,
+        `localhost:5984/db`,
+      ])
+      url = `http://${user}:${password}@${host}:${port}`
+    })
+
     it("test valid connection string", async () => {
       const result = await validator({
-        url: env.COUCH_DB_URL,
-        database: "",
+        url,
+        database: "db",
       })
       expect(result).toBe(true)
     })
 
     it("test invalid database", async () => {
       const result = await validator({
-        url: env.COUCH_DB_URL,
-        database: "db",
+        url,
+        database: "random_db",
       })
       expect(result).toBe(false)
     })
