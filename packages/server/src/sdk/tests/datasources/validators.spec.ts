3389aa4e9da52aaa237ef59aa22aabf330e61fd2
Adria Navarro <adria@budibase.com>
2023-05-11 20:32:44 +0200

    Wait for readiness

--- packages/server/src/sdk/tests/datasources/validators.spec.ts
+++ packages/server/src/sdk/tests/datasources/validators.spec.ts
@@ -1,7 +1,8 @@
 import { SourceName } from "@budibase/types"
 import integrations from "../../../integrations"
-import { GenericContainer } from "testcontainers"
+import { GenericContainer, Wait } from "testcontainers"
 import { generator } from "@budibase/backend-core/tests"
+import { Duration, TemporalUnit } from "node-duration"
 
 jest.unmock("pg")
 jest.unmock("mysql2/promise")
@@ -178,14 +179,24 @@ describe("datasource validators", () => {
 
     let host: string, port: number
 
+    const password = "Str0Ng_p@ssW0rd!"
+
     beforeAll(async () => {
       const container = await new GenericContainer(
         "mcr.microsoft.com/mssql/server"
       )
         .withExposedPorts(1433)
         .withEnv("ACCEPT_EULA", "Y")
-        .withEnv("MSSQL_SA_PASSWORD", "Str0Ng_p@ssW0rd!")
+        .withEnv("MSSQL_SA_PASSWORD", password)
         .withEnv("MSSQL_PID", "Developer")
+        .withWaitStrategy(Wait.forHealthCheck())
+        .withHealthCheck({
+          test: `/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${password}" -Q "SELECT 1" -b -o /dev/null`,
+          interval: new Duration(1000, TemporalUnit.MILLISECONDS),
+          timeout: new Duration(3, TemporalUnit.SECONDS),
+          retries: 20,
+          startPeriod: new Duration(100, TemporalUnit.MILLISECONDS),
+        })
         .start()
 
       host = container.getContainerIpAddress()
@@ -195,7 +206,7 @@ describe("datasource validators", () => {
     it("test valid connection string", async () => {
       const result = await validator({
         user: "sa",
-        password: "Str0Ng_p@ssW0rd!",
+        password,
         server: host,
         port: port,
         database: "master",
