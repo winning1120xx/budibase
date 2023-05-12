837e76372b8f807da25551eb9e2a603713d480c4
Adria Navarro <adria@budibase.com>
2023-05-12 12:59:59 +0200

    Implement the check as part of the integration

--- packages/server/src/sdk/tests/datasources/validators.spec.ts
+++ packages/server/src/sdk/tests/datasources/validators.spec.ts
@@ -8,6 +8,7 @@ import postgres from "../../../integrations/postgres"
 import mysql from "../../../integrations/mysql"
 import couchdb from "../../../integrations/couchdb"
 import mssql from "../../../integrations/microsoftSqlServer"
+import mongo from "../../../integrations/mongodb"
 
 jest.unmock("pg")
 jest.unmock("mysql2/promise")
@@ -151,8 +152,6 @@ describe("datasource validators", () => {
       url = `http://${user}:${password}@${host}:${port}`
     })
 
-    let url: string
-
     beforeAll(async () => {
       const user = generator.first()
       const password = generator.hash()
@@ -265,8 +264,6 @@ describe("datasource validators", () => {
   })
 
   describe("mongo", () => {
-    const validator = integrations.getValidator[SourceName.MONGODB]
-
     let connectionSettings: {
       user: string
       password: string
@@ -302,46 +299,50 @@ describe("datasource validators", () => {
     })
 
     it("test valid connection string", async () => {
-      const result = await validator({
+      const integration = new mongo.integration({
         connectionString: getConnectionString(),
         db: "",
         tlsCertificateFile: "",
         tlsCertificateKeyFile: "",
         tlsCAFile: "",
       })
+      const result = await integration.testConnection()
       expect(result).toBe(true)
     })
 
     it("test invalid password", async () => {
-      const result = await validator({
+      const integration = new mongo.integration({
         connectionString: getConnectionString({ password: "wrong" }),
         db: "",
         tlsCertificateFile: "",
         tlsCertificateKeyFile: "",
         tlsCAFile: "",
       })
+      const result = await integration.testConnection()
       expect(result).toEqual({ error: "Authentication failed." })
     })
 
     it("test invalid username", async () => {
-      const result = await validator({
+      const integration = new mongo.integration({
         connectionString: getConnectionString({ user: "wrong" }),
         db: "",
         tlsCertificateFile: "",
         tlsCertificateKeyFile: "",
         tlsCAFile: "",
       })
+      const result = await integration.testConnection()
       expect(result).toEqual({ error: "Authentication failed." })
     })
 
     it("test invalid connection", async () => {
-      const result = await validator({
+      const integration = new mongo.integration({
         connectionString: getConnectionString({ host: "http://nothinghere" }),
         db: "",
         tlsCertificateFile: "",
         tlsCertificateKeyFile: "",
         tlsCAFile: "",
       })
+      const result = await integration.testConnection()
       expect(result).toEqual({ error: "Error: getaddrinfo ENOTFOUND http" })
     })
   })
